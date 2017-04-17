93f136a5c9896e1157dd3e134b0a13cb68d04dff
Luke Robinson <robinsonlm@live.co.uk>
2017-04-17 12:52:11 +0100

    Initial map generator using a random or provided seed. No smoothing currently.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,16 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
+    public int width;
+    public int height;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string seed;
+    public bool useRandomSeed;
+
+    [Range(0, 100)]
+	public int randomFillPercent;
+	int[,] map;
+
+    void Start() {
+        GenerateMap();
+    }
+
+    void GenerateMap() {
+        map = new int[width, height];
+        RandomFillMap();
+    }
+
+    void RandomFillMap() {
+        if(useRandomSeed) {
+            seed = Time.time.ToString();
+        }
+
+        System.Random randomNumberGenerator = new System.Random(seed.GetHashCode());
+
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                map[x, y] = randomNumberGenerator.Next(0, 100) < randomFillPercent ? 1 : 0;
+            }
+        }
+    }
+
+    void OnDrawGizmos() {
+        if(map != null) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    Gizmos.color = map[x, y] == 1 ? Color.black : Color.white;
+                    Vector3 position = new Vector3((-width / 2) + x + 0.5f, 0, (-height / 2) + y + 0.5f);
+                    Gizmos.DrawCube(position, Vector3.one);
+                }
+            }
+        }
+    }
 }
